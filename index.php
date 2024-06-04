19666af380dbbf8446a377d1cc8e6c768aaf6a10
aldrichfcmw <aldrichmuktiwibiwo@gmail.com>
2024-06-04 16:20:26 +0700

    feat: create index

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+require_once("./Telebot.php");
+
+// initialize bot
+$bot = new Telebot("7465950918:AAExYVr4dM1Hwpo0rPBiw_4Fy1rKHAqrmyY");
+
+// handle start command
+$bot->command("start", function ($ctx) {
+    $ctx->replyWithText("Kamu mengirimkan command /start");
+});
+
+// handle hello command
+$bot->command("hello", function ($ctx) {
+    $ctx->replyWithText("Halo kak " . $ctx->from->first_name);
+});
+
+// run bot
+$bot->run();
